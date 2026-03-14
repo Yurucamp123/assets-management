b847664559b0e8937bbbe3936a65224cd731f3d2
binh <lethanhbinh6122003@gmail.com>
2026-03-15 00:37:24 +0700

    Fix some bugs typescript config

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080',
   withCredentials: true, // Luôn gửi kèm Cookie
   headers: {
     'Content-Type': 'application/json',
